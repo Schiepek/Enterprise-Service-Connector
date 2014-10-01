70c7716e805a29b10ce4dd9693fbadcac3451be6
dandreol <dandreol@hsr.ch>
2014-10-01 17:12:41 +0200

    SalesForceTokenResponse renamed

--- app/models/SalesForceTokenResponse.java
+++ app/models/SalesForceTokenResponse.java
@@ -2,7 +2,7 @@ package models;
 
 import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
 
-public class salesForceTokenResponse_old extends OAuthJSONAccessTokenResponse {
+public class SalesForceTokenResponse extends OAuthJSONAccessTokenResponse {
 
     public String getInstance() {
         return getParam("instance_url");
