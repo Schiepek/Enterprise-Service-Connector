28b7466c4f592b5df07d08430cf4a43b956c22b5
rschiepe <rschiepe@hsr.ch>
2014-10-21 13:24:34 +0200

    new fields v3

--- app/logic/gmail/GMailContactAccess.java
+++ app/logic/gmail/GMailContactAccess.java
@@ -21,11 +21,9 @@ public class GMailContactAccess {
     ContactsService service;
     private static final String GROUP_NAME = "salesforce";
     private static final String CONTACT_FEED_URL = "https://www.google.com/m8/feeds/contacts/default/full";
-    private static final String SCHEMA_WORK = "http://schemas.google.com/g/2005#work";
-    private static final String SCHEMA_MOBILE = "http://schemas.google.com/g/2005#mobile";
     private static final String CONTACT_QUERY = "https://www.google.com/m8/feeds/contacts/default/full?q=";
     private static final String GROUP_DEFAULT = "https://www.google.com/m8/feeds/groups/default/full";
-    private static final String SALESFORCE_INSTANCE = APIConfig.getAPIConfig(ServiceProvider.SALESFORCE).getInstance()+"/";
+    private static final String SALESFORCE_INSTANCE = APIConfig.getAPIConfig(ServiceProvider.SALESFORCE).getInstance() + "/";
     private static final String LAST_MODIFIED = "Last Modified";
 
     public GMailContactAccess() throws IOException {
@@ -38,8 +36,8 @@ public class GMailContactAccess {
         URL postUrl = new URL(CONTACT_FEED_URL);
         for (Contact contact : container.getContacts()) {
             ContactEntry entry = getContact(contact);
-            if(entry==null) service.insert(postUrl, createContactEntry(contact, groupId));
-            else if(hasNewValues(entry, contact)) service.update(postUrl, createContactEntry(contact, groupId));
+            if (entry == null) service.insert(postUrl, createContactEntry(contact, groupId));
+            else if (hasNewValues(entry, contact)) service.update(postUrl, createContactEntry(contact, groupId));
         }
     }
 
@@ -48,17 +46,17 @@ public class GMailContactAccess {
         Query myQuery = new Query(feedUrl);
         ContactFeed resultFeed = service.query(myQuery, ContactFeed.class);
         List<ContactEntry> resultList = resultFeed.getEntries();
-        if(resultList.isEmpty()) return null;
+        if (resultList.isEmpty()) return null;
         return resultFeed.getEntries().get(0);
     }
 
     private boolean hasNewValues(ContactEntry entry, Contact c) throws ServiceException, java.text.ParseException, IOException {
         String transferDate = EscDateTimeParser.parseSfDateToString((c.getLastModifiedDate()));
         String googleDate = null;
-        for(UserDefinedField field : entry.getUserDefinedFields()) {
+        for (UserDefinedField field : entry.getUserDefinedFields()) {
             if (field.getKey().equals(LAST_MODIFIED)) googleDate = field.getValue();
         }
-        if (googleDate==null) return true;
+        if (googleDate == null) return true;
         System.out.println(transferDate + "//" + googleDate);
         return !transferDate.equals(googleDate);
     }
@@ -67,61 +65,87 @@ public class GMailContactAccess {
         ContactEntry entry = new ContactEntry();
         entry.addGroupMembershipInfo(new GroupMembershipInfo(false, groupId));
         createName(entry, c);
-        createMail(entry, c);
-        createWebsite(entry, c);
+     //   createMail(entry, c);
+        createLinks(entry, c);
         createBirthday(entry, c);
         createLanguage(entry, c);
-        createPhoneNumber(entry, c.getPhone(), SCHEMA_WORK, true);
-        createPhoneNumber(entry, c.getMobilePhone(), SCHEMA_MOBILE, false);
+        createPhoneNumber(entry, c.getPhone(), PhoneNumber.Rel.WORK, true);
+        createPhoneNumber(entry, c.getMobilePhone(), PhoneNumber.Rel.MOBILE, false);
         createPostalAddress(entry, c);
         createTitle(entry, c);
         createManager(entry, c);
         createFcontact(entry, c);
         createOrganization(entry, c);
-        createId(entry, c);
-        createLastModified(entry ,c);
+        createLastModified(entry, c);
         return entry;
     }
 
     private void createName(ContactEntry entry, Contact c) {
-        if (c.getFirstName() == null && c.getLastName()==null) return;
+        if (c.getLastName() == null) return;
         Name name = new Name();
-        final String NO_YOMI = null;
-        name.setNamePrefix(new NamePrefix(c.getSalutation()));
-        name.setFullName(new FullName(c.getFirstName() + " " + c.getLastName(), NO_YOMI));
-        name.setGivenName(new GivenName(c.getFirstName(), NO_YOMI));
-        name.setFamilyName(new FamilyName(c.getLastName(), NO_YOMI));
+        if(c.getFirstName()!=null) {
+            name.setGivenName(new GivenName(c.getFirstName(), null));
+            name.setFullName(new FullName(c.getFirstName() + " " + c.getLastName(), null));
+        } else { name.setFullName(new FullName(c.getLastName(), null)); }
+        name.setFamilyName(new FamilyName(c.getLastName(), null));
         entry.setName(name);
     }
 
+    private void updateName(ContactEntry entry, Contact c) {
+        Name name = entry.getName();
+        if (name == null) createName(entry, c);
+        name.getFullName().setValue(c.getFirstName() + " " + c.getLastName());
+        name.getGivenName().setValue(c.getFirstName());
+        name.getFamilyName().setValue(c.getLastName());
+    }
+
     private void createMail(ContactEntry entry, Contact c) {
-        if (c.getEmail()==null) return;
+        if (c.getEmail() == null) return;
         Email email = new Email();
         email.setAddress(c.getEmail());
-        if(c.getFirstName()!=null && c.getLastName()!=null) email.setDisplayName(c.getFirstName() + " " + c.getLastName());
-        email.setRel(SCHEMA_WORK);
+        if (c.getFirstName() != null && c.getLastName() != null) email.setDisplayName(c.getFirstName() + " " + c.getLastName());
+        email.setRel(Email.Rel.GENERAL);
         email.setPrimary(true);
         entry.addEmailAddress(email);
     }
 
-    private void createWebsite(ContactEntry entry, Contact c) {
-        //Überprüfung ob wesbsite==null noch einfügen
-        Website website = new Website();
-        website.setHref("www.example.com");
-        website.setPrimary(true);
-        website.setRel(Website.Rel.WORK);
-        entry.addWebsite(website);
+    private void updateMail(ContactEntry entry, Contact c) {
+        if (entry.getEmailAddresses().isEmpty()) createMail(entry, c);
+        Email email = entry.getEmailAddresses().get(0);
+        email.setDisplayName(c.getFirstName() + " " + c.getLastName());
+        email.setAddress(c.getEmail());
+    }
+
+    private void createLinks(ContactEntry entry, Contact c) {
+        if (c.getId()!=null) {
+            Website id = new Website();
+            id.setHref(SALESFORCE_INSTANCE  + c.getId());
+            id.setLabel("Salesforce Id");
+            id.setPrimary(true);
+            entry.addWebsite(id);
+        }
+        if (true) {
+            Website home = new Website();
+            home.setHref("www.blabla.com");
+            home.setLabel("Homepage");
+            home.setPrimary(false);
+            entry.addWebsite(home);
+        }
+    }
+
+    private void updateWebsite(ContactEntry entry, Contact c) {
+        //
     }
 
     private void createBirthday(ContactEntry entry, Contact c) {
-        if (c.getBirthdate()==null)  return;
+        if (c.getBirthdate() == null) return;
         Birthday birthday = new Birthday();
         birthday.setWhen(c.getBirthdate());
         entry.setBirthday(birthday);
     }
 
     private void createLanguage(ContactEntry entry, Contact c) {
-        if(c.getLanguages()==null) return;
+        if (c.getLanguages() == null) return;
         if (!Arrays.asList(Locale.getISOLanguages()).contains(c.getLanguages())) return;
         Language lang = new Language();
         lang.setCode(c.getLanguages());
@@ -129,7 +153,7 @@ public class GMailContactAccess {
     }
 
     private void createPhoneNumber(ContactEntry entry, String number, String schema, boolean primary) {
-        if (number==null) return;
+        if (number == null) return;
         PhoneNumber phoneNumber = new PhoneNumber();
         phoneNumber.setPhoneNumber(number);
         phoneNumber.setRel(schema);
@@ -143,36 +167,35 @@ public class GMailContactAccess {
         String plz = c.getMailingPostalCode();
         String country = c.getMailingCountry();
         StructuredPostalAddress postalAddress = new StructuredPostalAddress();
-        if(street==null && city==null && plz==null && country==null) return;
+        if (street == null && city == null && plz == null && country == null) return;
         postalAddress.setStreet(new Street(street));
         postalAddress.setCity(new City(city));
         postalAddress.setPostcode(new PostCode(plz));
-        Country cy= new Country();
+        Country cy = new Country();
         cy.setValue(country);
         postalAddress.setCountry(cy);
-        postalAddress.setFormattedAddress(new FormattedAddress(street + " " + plz + " " + city + " " + country));
-        createFormattedAddresss(postalAddress, street, plz, city, country);
-        postalAddress.setRel(SCHEMA_WORK);
+        postalAddress.setFormattedAddress(new FormattedAddress(createFormattedAddresss(street, plz, city, country)));
+        postalAddress.setRel(StructuredPostalAddress.Rel.WORK);
         postalAddress.setPrimary(true);
         entry.addStructuredPostalAddress(postalAddress);
     }
 
-    private void createFormattedAddresss(StructuredPostalAddress postalAddress, String street, String plz, String city, String country) {
+    private String createFormattedAddresss(String street, String plz, String city, String country) {
         String address = "";
         if (street != null) address += street;
         if (plz != null) address += " " + plz;
-        if(city != null) address += " " + city;
+        if (city != null) address += " " + city;
         if (country != null) address += " " + country;
-        if (address != "") postalAddress.setFormattedAddress(new FormattedAddress(address));
+        return address;
     }
 
-    private void createTitle(ContactEntry entry, Contact c)  {
-        if(c.getTitle()==null) return;
-        entry.setOccupation(new Occupation(c.getTitle()));
+    private void createTitle(ContactEntry entry, Contact c) {
+        if (c.getSalutationTitle()==null) return;
+        entry.setTitle(new PlainTextConstruct(c.getSalutationTitle()));
     }
 
     private void createManager(ContactEntry entry, Contact c) {
-        if(c.getReportsToId()==null) return;
+        if (c.getReportsToId() == null) return;
         Relation rel = new Relation();
         rel.setRel(Relation.Rel.MANAGER);
         //Die ID ist hier nicht optimal
@@ -181,12 +204,12 @@ public class GMailContactAccess {
     }
 
     private void createFcontact(ContactEntry entry, Contact c) {
-        if (c.getF_contact()==null) return;
+        if (c.getF_contact() == null) return;
         entry.addUserDefinedField(new UserDefinedField("F-Contact", c.getF_contact()));
     }
 
     private void createOrganization(ContactEntry entry, Contact c) {
-        if (c.getAccountId()==null) return;
+        if (c.getAccountId() == null) return;
         Organization org = new Organization();
         org.setRel(Organization.Rel.WORK);
         org.setOrgJobDescription(new OrgJobDescription(c.getTitle()));
@@ -194,17 +217,8 @@ public class GMailContactAccess {
         entry.addOrganization(org);
     }
 
-    private void createId(ContactEntry entry, Contact c) {
-        if(c.getId()==null) return;
-        Website website = new Website();
-        website.setHref(SALESFORCE_INSTANCE + c.getId());
-        website.setPrimary(false);
-        website.setLabel("Salesforce Id");
-        entry.addWebsite(website);
-    }
-
     private void createLastModified(ContactEntry entry, Contact c) throws ParseException, java.text.ParseException {
-        if(c.getLastModifiedDate()==null) return;
+        if (c.getLastModifiedDate() == null) return;
         UserDefinedField field = new UserDefinedField();
         field.setKey(LAST_MODIFIED);
         field.setValue(EscDateTimeParser.parseSfDateToString(c.getLastModifiedDate()));
