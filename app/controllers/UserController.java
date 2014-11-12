2e8771536d520aa28d12d35265e20b13f016206a
Dario Andreoli <mail@darioandreoli.ch>
2014-11-12 15:14:09 +0100

    created empty UserController

---
+++ app/controllers/UserController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+
+import models.Logging;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.log;
+
+public class UserController extends Controller {
+
+}
